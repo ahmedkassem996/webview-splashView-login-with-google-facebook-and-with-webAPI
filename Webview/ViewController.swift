1f9269b0d84c6797e7def6a4912e967c22ffe875
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-06-23 17:37:51 +0200

    Initial Commit

---
+++ Webview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Webview
+//
+//  Created by AHMED on 4/2/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ WebviewTests/WebviewTests.swift
@@ -0,0 +1,34 @@
+//
+//  WebviewTests.swift
+//  WebviewTests
+//
+//  Created by AHMED on 4/2/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import XCTest
+@testable import Webview
+
+class WebviewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
